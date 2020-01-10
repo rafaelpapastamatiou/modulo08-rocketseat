3a6d50ad6f084df79f14f6494d76b31d36e5797c
Rafael Papastamatiou <rafaelpapas1412@gmail.com>
2020-01-10 02:16:12 -0300

    Modulo 08 - ReactJS - Rocketseat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
-  return <h1>Hello React</h1>;
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }, [newTech, techs]);
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('techs');
+    if (storageTech) setTechs(JSON.parse(storageTech));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
+  const techSize = useMemo(() => techs.length, [techs]);
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
